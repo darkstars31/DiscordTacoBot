2ea2f45b3b91a0e1b198ee0c7dc41de7bab18d02
tony.santi@equipmentshare.com <darkstars31@gmail.com>
2022-01-08 21:12:14 -0600

    Guild Saving

--- app/dao/guildDao.js
+++ app/dao/guildDao.js
@@ -1,14 +1,15 @@
 import { dbContext } from './dbContext.js';
+import { log } from '../utils/logger.js';
 
 export const getGuild = async ( id ) => {
     const sql =`SELECT * FROM guilds WHERE guildId = ${id}`;
 	const db = await dbContext();
-	return db.get(sql, {}, err => console.error( err ));
+	return db.get(sql, {}, err => log.error( err ));
 }
 
 export const createGuild = async ( guild ) => {
     const sql =`INSERT INTO guilds (guildId, name, metadata) 
     VALUES ('${guild.id}',${guild.name},'${JSON.stringify(guild)}')`;
 	const db = await dbContext();
-	return db.run(sql, {}, err => console.error( err ));
+	return db.run(sql, {}, err => log.error( err ));
 }
--- app/service/tacoProcessor.js
+++ app/service/tacoProcessor.js
@@ -2,18 +2,22 @@ import { getUserIdsFromContent } from '../utils/messageParser.js';
 import { saveTaco, getTacosSentInLastDay } from '../dao/dao.js';
 import { createUser, getUser } from '../dao/userDao.js';
 import { saveViolation, getViolationsRowId } from '../dao/violationDao.js';
+import { createGuild } from "../dao/guildDao.js";
 import { log } from '../utils/logger.js';
 
 const DAILY_TACO_LIMIT_PER_USER = 3;
 
 export async function captureSentTacos( client, message ) {
-    const { author, content } = message;
+    const { author, content, guildId } = message;
         if( content.includes('@') && content.includes('🌮')){
             const userIdList = getUserIdsFromContent( message );
     
             // if( await selfGratificationViolation( userIdList, author)) {
             //     return;
             // }
+            log.debug('message.mentions', message.mentions);
+            const guild = await client.guilds.fetch( guildId );
+            createGuild( guild );
 
             const numTacosSentByAuthorInLastDay = (await getTacosSentInLastDay(author.id)).length;
 
@@ -22,20 +26,15 @@ export async function captureSentTacos( client, message ) {
             const usersFetchedFromDiscord = await Promise.all( 
                 userIdList.filter(userId => !usersFromDatabase.filter(Boolean).map( user => user.userId ).includes(userId))
                 .map( async userId => await client.users.fetch( userId )));
-            log.debug( usersFetchedFromDiscord );
             
-            if( usersFetchedFromDiscord ) {
-                usersFetchedFromDiscord.forEach( async user => await createUser( user ));
-            }
-
+            usersFetchedFromDiscord.forEach( async user => await createUser( user ));
+            
             const users = [...usersFromDatabase.filter(Boolean), ...usersFetchedFromDiscord.filter(Boolean)];
 
-            if( users.length > 0 ) {
-                users.forEach( user => {
-                    saveTaco( message, user);
-                    log.info( `>> ${author.username}#${user.discriminator} gave a taco to ${user.username}#${user.discriminator}` );
-                });
-            }
+            users.forEach( user => {
+                saveTaco( message, user);
+                log.info( `>> ${author.username}#${user.discriminator} gave a taco to ${user.username}#${user.discriminator}` );
+            });
    
                 // author.send(`Aww crap, you exceeded the maximum taco limit today (${DAILY_TACO_LIMIT_PER_USER}).\n
                 //             New (fresh) tacos will be arriving shortly, thank you for patience.`);   
